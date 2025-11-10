1f7f444902705594ba59db59c933d0627ef1bf02
YaeZed <1511623546@qq.com>
2025-11-10 20:56:36 +0800

    reavticeFamily

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <!-- <commandBasic></commandBasic> -->
-  <refFamily></refFamily>
+  <!-- <refFamily></refFamily> -->
+  <reactiveFamily></reactiveFamily>
 </template>
 
 <script setup lang="ts">
 import commandBasic from "./components/commandBasic.vue";
 import refFamily from "./components/refFamily.vue";
+import reactiveFamily from "./components/reactiveFamily.vue";
 </script>
 
 <style scoped></style>
